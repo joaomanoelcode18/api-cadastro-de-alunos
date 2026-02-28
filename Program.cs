c9efdbfa984b7b261aa7f0bf86f5f923ff69bbf6
joao <joaomanoel2006f@gmail.com>
2026-02-27 21:01:55 -0300

    ajuste

--- Program.cs
+++ Program.cs
@@ -144,6 +144,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
+app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.Run();
